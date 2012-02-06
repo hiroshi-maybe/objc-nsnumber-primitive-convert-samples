0e36bc8e70e374fe780da9b5a9f2773af976750b
Hiroshi Kori <kori39@gmail.com>
2012-02-06 17:31:14 +0900

    Initial Commit

---
+++ nsnumber-primitive-convert-demo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  nsnumber-primitive-convert-demo
+//
+//  Created by 宏志 郡 on 12/02/06.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
